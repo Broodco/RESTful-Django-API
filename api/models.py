218426addcf9d69dca9c1b2f1ce7c2b20875104a
Broodco <broodcoorens.maxime@gmail.com>
2019-06-21 11:31:31 +0200

    Add Model and fixed Settings

---
+++ api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Application(models.Model):
+    application = models.FileField(upload_to=applications)
+    package_name = models.Charfield(max_length=120)
+    package_version_code = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
