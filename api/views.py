f6bf8cbf2d407d81d5df8ed0e3a03658529b7352
Broodco <broodcoorens.maxime@gmail.com>
2019-06-21 16:11:18 +0200

    Add API root

--- api/views.py
+++ api/views.py
@@ -1,8 +1,17 @@
 from rest_framework import viewsets, mixins
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from .models import Application
 from .serializers import ApplicationSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def api_root(request, format = None):
+    return Response({
+        'applications':reverse('application-list',request=request,format=format)
+    })
+
 class ApplicationView(viewsets.ModelViewSet):
     queryset = Application.objects.all()
     serializer_class = ApplicationSerializer
